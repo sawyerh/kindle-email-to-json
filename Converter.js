54014b09199248673fbc03a61e36b3eeb265dad2
Sawyer Hollenshead <git@sawyerh.com>
2021-01-02 20:15:58 -0500

    Add support for non-English exports

--- Converter.js
+++ Converter.js
@@ -5,7 +5,7 @@ const cheerio = require("cheerio");
  * @param {String} html
  * @returns {Object}
  */
-const Converter = function(html) {
+const Converter = function (html) {
   this.html = html;
   this.$ = cheerio.load(this.html);
 };
@@ -14,7 +14,7 @@ const Converter = function(html) {
  * Determine whether the given HTML is a valid Kindle notes export
  * @returns {Boolean}
  */
-Converter.prototype.valid = function() {
+Converter.prototype.valid = function () {
   if (this.html) {
     const notes = this.$(".noteText");
     return notes.length > 0;
@@ -27,21 +27,21 @@ Converter.prototype.valid = function() {
  * Parse the HTML to pull out the volume's title, author, and highlights
  * @returns {Object}
  */
-Converter.prototype.getJSON = function() {
+Converter.prototype.getJSON = function () {
   const titleEl = this.$(".bookTitle");
   const authorEl = this.$(".authors");
   const title = titleEl.text().trim();
   const authors = authorEl
     .text()
     .split(";")
-    .map(s => s.trim());
+    .map((s) => s.trim());
 
   return {
     volume: {
       title: title,
-      authors: authors
+      authors: authors,
     },
-    highlights: this.highlights()
+    highlights: this.highlights(),
   };
 };
 
@@ -49,34 +49,29 @@ Converter.prototype.getJSON = function() {
  * Parse the highlights and notes from the HTML
  * @returns {Array} highlights
  */
-Converter.prototype.highlights = function() {
+Converter.prototype.highlights = function () {
   const headings = this.$(".noteHeading");
   let highlights = [];
 
-  headings.each((index, el) => {
+  headings.each((_index, el) => {
     const heading = cheerio(el);
-    const color = heading
-      .find("span[class^='highlight_']")
-      .text()
-      .trim();
+    const color = heading.find("span[class^='highlight_']").text().trim();
     const text = heading.text().trim();
 
-    const location = text.match(/location\s(\d*)/i);
+    const location = text.match(/\s(\d*)$/i) || "";
 
-    if (location) {
-      if (text.match(/^Note -/i)) {
-        // We're making the assumption that notes are only added on top of
-        // a highlight. When that's the case, the exported file will include
-        // the note directly after the text it's added on.
-        if (highlights.length) {
-          const highlight = highlights[highlights.length - 1];
-          highlight.notes = this.highlightContent(location[1], color, el);
-        }
-      } else {
-        highlights = highlights.concat(
-          this.highlightContent(location[1], color, el)
-        );
-      }
+    // We need to differentiate between a "highlight" and a "note." The heading
+    // for the latter case doesn't have a color element as a child
+    if (!color && highlights.length) {
+      // We're making the assumption that notes are only added on top of
+      // a highlight. When that's the case, the exported file will include
+      // the note directly after the text it's added on.
+      const highlight = highlights[highlights.length - 1];
+      highlight.notes = this.highlightContent(location[1], color, el);
+    } else {
+      highlights = highlights.concat(
+        this.highlightContent(location[1], color, el)
+      );
     }
   });
 
@@ -90,17 +85,15 @@ Converter.prototype.highlights = function() {
  * @param  {Node} el
  * @return {Array} The parsed highlight objects
  */
-Converter.prototype.highlightContent = function(location, color, el) {
+Converter.prototype.highlightContent = function (location, color, el) {
   let highlights = [];
   const nextEl = cheerio(el).next();
 
   if (nextEl.hasClass("noteText")) {
     const highlight = {
       color: color,
-      content: cheerio(nextEl)
-        .text()
-        .trim(),
-      location: location
+      content: cheerio(nextEl).text().trim(),
+      location: location,
     };
 
     highlights.push(highlight);
